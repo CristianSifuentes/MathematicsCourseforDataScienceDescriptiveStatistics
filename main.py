977b04ec8981e1caee04f1ba38a39ff971dd91e6
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-02-28 17:34:01 -0600

    Another example for mean, median, head using pandas

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pandas as pd 
+import os
 
-df = pd.read_csv('cars.csv')
 
-df
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../MathematicsCourseforDataScienceDescriptiveStatistics/cars.csv"))
+print(df.dtypes)
+
+
+print(df.describe())
+
+
+print(df.head())
+
+print(df['price_usd'].mean())
+
+
+print(df['price_usd'].median())
+
+print(df['price_usd'].plot.hist(bins=20))
