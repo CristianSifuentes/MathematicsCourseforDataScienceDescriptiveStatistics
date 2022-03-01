d1c0300b80cba7b43f5ce19de196062aa7397237
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-03-01 17:01:13 -0600

    examples for outliers (Q1, Q2 and Q3)

--- measuresofdispersion.py
+++ measuresofdispersion.py
@@ -24,3 +24,22 @@ print(min_val, Q1, median, Q3, max_val)
 iqr = Q3 - Q1
 print(iqr)
 
+
+# Límites para detección de outliers (datos simetricamente distribuidos)
+
+# Datos entre Q1 −1.5 × IQR y Q3 +1.5 x IQR
+
+minlimit = Q1 - 1.5*iqr
+maxlimit = Q3 + 1.5*iqr
+print('rango para detección de outliers: {}, {}'.format(minlimit, maxlimit))
+
+
+sns.set(rc={'figure.figsize':(11.7,8.27)})
+f, (ax_hist, ax_box) = plt.subplots(2, sharex=True, gridspec_kw={"height_ratios": (.6, .4)})
+sns.histplot(df['price_usd'], ax=ax_hist)
+sns.boxplot(df['price_usd'], ax=ax_box)
+ax_hist.set(xlabel='')
+
+# Es posible calcular varios box-plot separando por una cierta variable categórica:
+
+sns.boxplot(x = 'engine_fuel', y = 'price_usd', data = df)
