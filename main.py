47daa9cff9f5909324f32a04bebe7b7057cd307e
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-03-01 14:55:24 -0600

    New example for boxplot using seaborn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import os
 import seaborn as sns
-
+import matplotlib.pyplot as plt
 
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../MathematicsCourseforDataScienceDescriptiveStatistics/cars.csv"))
 print(df.dtypes)
@@ -12,11 +12,13 @@ print(df.describe())
 
 print(df.head())
 
+# promedio
 print(df['price_usd'].mean())
 
-
+# mediana
 print(df['price_usd'].median())
 
+#
 print(df['price_usd'].plot.hist(bins=20))
 
 print(sns.displot(df, x = 'price_usd', hue = 'manufacturer_name'))
@@ -31,3 +33,8 @@ print(df.groupby('engine_type').count())
 Q7_df = df[(df['manufacturer_name']=='Audi') & (df['model_name']=='Q7')]
 sns.histplot(Q7_df, x='price_usd', hue = 'year_produced')
 print(Q7_df)
+
+
+sns.set_style("whitegrid")
+plt.figure(figsize=(12, 8))
+sns.boxplot(x='color', y='price_usd', data=df, palette='Blues')
