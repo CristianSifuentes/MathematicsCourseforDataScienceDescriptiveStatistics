9ea927f57c7663df310b17e2d1259c04982b631c
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2022-02-28 22:23:51 -0600

    Adding advanced examples using pandas

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # MathematicsCourseforDataScienceDescriptiveStatistics
+
+
+pip install pandas
+pip install seaborn
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import os
+import seaborn as sns
 
 
 df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../MathematicsCourseforDataScienceDescriptiveStatistics/cars.csv"))
@@ -17,3 +18,16 @@ print(df['price_usd'].mean())
 print(df['price_usd'].median())
 
 print(df['price_usd'].plot.hist(bins=20))
+
+print(sns.displot(df, x = 'price_usd', hue = 'manufacturer_name'))
+
+print(sns.displot(df, x="price_usd", hue="engine_type"))
+
+print(sns.displot(df, x='price_usd', hue = 'engine_type', multiple='stack'))
+
+print(df.groupby('engine_type').count())
+
+
+Q7_df = df[(df['manufacturer_name']=='Audi') & (df['model_name']=='Q7')]
+sns.histplot(Q7_df, x='price_usd', hue = 'year_produced')
+print(Q7_df)
